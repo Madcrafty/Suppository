0857ad91e41515bb83e478186a69807e9316cb8b
Thorfish <yhal.liam@gmail.com>
2024-05-14 11:40:02 +1000

    Cleanup

--- brush/parameters.js
+++ brush/parameters.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as build from './build.js';
 //These are the brush parameters
 //Just use import { parameters } from "/brush/parameters.js"
 //use parameters.variable to access a particular variable.
@@ -9,5 +10,8 @@ export const parameters = {
     brushHeight:0,
     brushKern:30,
     brushColor:new THREE.Color(0,255,0),
-    brushShine:0
+    brushShine:0,
+    update() {
+        build.createBrush();
+    }
 }
--- src/rete/default.ts
+++ src/rete/default.ts
@@ -25,14 +25,12 @@ import {
 } from 'rete-context-menu-plugin';
 
 import {parameters} from '../../brush/parameters.js';
-import * as build from '../../brush/build.js';
 import * as THREE from 'three';
 
-type Node = NumberNode | AddNode;
+type Node = NumberNode | BrushNode;
 type Conn =
-  | Connection<NumberNode, AddNode>
-  | Connection<AddNode, AddNode>
-  | Connection<AddNode, NumberNode>;
+  | Connection<NumberNode, BrushNode>
+  | Connection<BrushNode, NumberNode>;
 type Schemes = GetSchemes<Node, Conn>;
 
 class Connection<A extends Node, B extends Node> extends Classic.Connection<
@@ -63,12 +61,12 @@ class NumberNode extends Classic.Node implements DataflowNode {
   }
 }
 
-class AddNode extends Classic.Node implements DataflowNode {
+class BrushNode extends Classic.Node implements DataflowNode {
   width = 180;
   height = 400;
 
   constructor() {
-    super('Add');
+    super('Brush');
 
     this.addInput('a', new Classic.Input(socket, 'Size'));
     this.addInput('b', new Classic.Input(socket, 'Alpha'));
@@ -79,16 +77,9 @@ class AddNode extends Classic.Node implements DataflowNode {
     this.addInput('f', new Classic.Input(socket, 'Red'));
     this.addInput('g', new Classic.Input(socket, 'Green'));
     this.addInput('h', new Classic.Input(socket, 'Blue'));
-
-    this.addOutput('value', new Classic.Output(socket, 'Number'));
-    this.addControl(
-      'result',
-      new Classic.InputControl('number', { initial: 0, readonly: true })
-    );
   }
   data(inputs: { a?: number[]; b?: number[]; c?: number[]; d?: number[]; e?: number[]; f?: number[]; g?: number[]; h?: number[];}) {
     const { a = [], b = [], c = [],d = [],e = [],f = [],g = [],h = [],} = inputs;
-    const sum = (a[0] || 0) + (b[0] || 0);
     parameters.brushSize = (a[0]!=undefined) ? a[0] : parameters.brushSize;
     parameters.brushAlpha = (b[0]!=undefined) ? b[0] : parameters.brushAlpha;
     parameters.brushHeight = (c[0]!=undefined) ? c[0] : parameters.brushHeight;
@@ -96,38 +87,10 @@ class AddNode extends Classic.Node implements DataflowNode {
     parameters.brushShine = (e[0]!=undefined) ? e[0] : parameters.brushShine;
     // color
     parameters.brushColor = (f[0]!=undefined && g[0]!=undefined && h[0]!=undefined) ? new THREE.Color(f[0],g[0],h[0]) : parameters.brushColor;
-    build.createBrush();
-    (this.controls['result'] as Classic.InputControl<'number'>).setValue(sum);
+    parameters.update();
 
     return {
-      value: sum,
-    };
-  }
-}
-
-class BrushOutputNode extends Classic.Node implements DataflowNode {
-  width = 180;
-  height = 195;
-
-  constructor() {
-    super('Brush');
-
-    this.addInput('a', new Classic.Input(socket, 'Size'));
-    this.addInput('b', new Classic.Input(socket, 'B'));
-    this.addOutput('value', new Classic.Output(socket, 'Number'));
-    this.addControl(
-      'result',
-      new Classic.InputControl('number', { initial: 0, readonly: true })
-    );
-  }
-  data(inputs: { a?: number[]; b?: number[] }) {
-    const { a = [], b = [] } = inputs;
-    console.log("get a load of this:" + a[0]);
-    parameters.brushSize = a[0];
-    (this.controls['result'] as Classic.InputControl<'number'>).setValue(parameters.brushSize);
-
-    return {
-      value: parameters.brushSize,
+      value: 1,
     };
   }
 }
@@ -137,23 +100,23 @@ type AreaExtra = Area2D<Schemes> | ReactArea2D<Schemes> | ContextMenuExtra;
 const socket = new Classic.Socket('socket');
 
 export async function createEditor(container: HTMLElement) {
+  //Core
   const editor = new NodeEditor<Schemes>();
   const area = new AreaPlugin<Schemes, AreaExtra>(container);
   const connection = new ConnectionPlugin<Schemes, AreaExtra>();
   const reactRender = new ReactPlugin<Schemes, AreaExtra>({ createRoot });
-
+  const dataflow = new DataflowEngine<Schemes>();
   const contextMenu = new ContextMenuPlugin<Schemes>({
     items: ContextMenuPresets.classic.setup([
       ['Number', () => new NumberNode(1, process)],
-      ['Add', () => new AddNode()],
-      ['BrushOutput', () => new BrushOutputNode()],
+      ['Brush', () => new BrushNode()],
     ]),
   });
 
+  //References
   editor.use(area);
-
+  editor.use(dataflow);
   area.use(reactRender);
-
   area.use(connection);
   area.use(contextMenu);
 
@@ -161,61 +124,49 @@ export async function createEditor(container: HTMLElement) {
   reactRender.addPreset(ReactPresets.classic.setup());
   reactRender.addPreset(ReactPresets.contextMenu.setup());
 
-  const dataflow = new DataflowEngine<Schemes>();
+  //Create Starting Nodes
+  const a = new NumberNode(5, process);
+  const b = new NumberNode(100, process);
+  const brush = new BrushNode();
 
-  editor.use(dataflow);
-
-  const a = new NumberNode(1, process);
-  const b = new NumberNode(1, process);
-  const add = new AddNode();
-
+  //Add Nodes and Connections to Editor
   await editor.addNode(a);
   await editor.addNode(b);
-  await editor.addNode(add);
+  await editor.addNode(brush);
+  await editor.addConnection(new Connection(a, 'value', brush, 'a'));
+  await editor.addConnection(new Connection(b, 'value', brush, 'b'));
 
-  await editor.addConnection(new Connection(a, 'value', add, 'a'));
-  await editor.addConnection(new Connection(b, 'value', add, 'b'));
-
+  //Arrange
   const arrange = new AutoArrangePlugin<Schemes>();
-
   arrange.addPreset(ArrangePresets.classic.setup());
-
   area.use(arrange);
-
   await arrange.layout();
 
+  //Position Camera Near Nodes
   AreaExtensions.zoomAt(area, editor.getNodes());
-
   AreaExtensions.simpleNodesOrder(area);
 
+  //Selecting
   const selector = AreaExtensions.selector();
   const accumulating = AreaExtensions.accumulateOnCtrl();
-
   AreaExtensions.selectableNodes(area, selector, { accumulating });
-// make data function calls here
+
+// This function basically updates each node that connects to a BrushNode.
+// Will need an upgrade soon!
   async function process() {
     dataflow.reset();
 
     editor
       .getNodes()
-      .filter((node) => node instanceof AddNode)
+      .filter((node) => node instanceof BrushNode)
       .forEach(async (node) => {
-        const sum = await dataflow.fetch(node.id);
-
-        console.log(node.id, 'produces', sum);
-
-        area.update(
-          'control',
-          (node.controls['result'] as Classic.InputControl<'number'>).id
-        );
+        //Retrieve / Update the Data of the node
+        await dataflow.fetch(node.id);
       });
   }
 
   editor.addPipe((context) => {
-    if (
-      context.type === 'connectioncreated' ||
-      context.type === 'connectionremoved'
-    ) {
+    if (context.type === 'connectioncreated' || context.type === 'connectionremoved') {
       process();
     }
     return context;
