5f08c6000d8e1e9ad1a10d43f90bb1b6b0315116
Thorfish <yhal.liam@gmail.com>
2024-05-29 17:04:40 +1000

    alpha

--- src/nodes/OutputNode.ts
+++ src/nodes/OutputNode.ts
@@ -12,11 +12,14 @@ export class OutputNode extends Classic.Node {
       this.addInput('texture', new Classic.Input(sockets.tex, 'Texture'));
       this.addInput('height', new Classic.Input(sockets.tex, 'Height'));
       this.addInput('shine', new Classic.Input(sockets.tex, 'Shine'));
+      this.addInput('alpha', new Classic.Input(sockets.tex, 'Alpha'));
+
     }
     data(inputs: any) {
       material.brushTexture = inputs['texture'] && inputs['texture'][0] instanceof Int16Array ? utils.int16touint8(inputs['texture'][0]) : material.brushTexture;
       material.heightTexture = inputs['height'] && inputs['height'][0] instanceof Int16Array ? inputs['height'][0] : material.heightTexture;
       material.shineTexture = inputs['shine'] && inputs['shine'][0] instanceof Int16Array ? inputs['shine'][0] : material.shineTexture;
+      material.alphaTexture = inputs['alpha'] && inputs['alpha'][0] instanceof Int16Array ? inputs['alpha'][0] : material.alphaTexture;
       return {};
     }
   }
